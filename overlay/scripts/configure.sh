16f31a93b013fb4f80d711ac68f253b08113603d
Henrik Hautakoski <henrik@eossweden.org>
2021-11-15 13:47:57 +0100

    overlay/scripts/configure.sh: Add support for running a single configure file if passed as argument.

--- overlay/scripts/configure.sh
+++ overlay/scripts/configure.sh
@@ -4,6 +4,11 @@ BASEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source ${BASEPATH}/functions.sh
 
+if [ $# -gt 0 ]; then
+    configure "${BASEPATH}/conf.d/$@.sh"
+    exit
+fi
+
 for script in $(find ${BASEPATH}/conf.d -type f -print | sort); do
     configure "$script"
 done
