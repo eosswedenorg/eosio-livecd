d32f968321a184ffce57ae68c3983f37945b4e41
Henrik Hautakoski <henrik@eossweden.org>
2021-11-15 13:47:24 +0100

    overlay/scripts/configure.sh: use configure function.

--- overlay/scripts/configure.sh
+++ overlay/scripts/configure.sh
@@ -5,6 +5,5 @@ BASEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${BASEPATH}/functions.sh
 
 for script in $(find ${BASEPATH}/conf.d -type f -print | sort); do
-	echo -e " [\e[1;34m*\e[0m] Running $(basename ${script})"
-    source $script
+    configure "$script"
 done
