477c82f0844d41757a607d07b0e5cc3b86668a97
Henrik Hautakoski <henrik@eossweden.org>
2021-11-15 17:51:57 +0100

    overlay/scripts/init.sh: update ca-certificates

--- overlay/scripts/init.sh
+++ overlay/scripts/init.sh
@@ -7,7 +7,7 @@ mount none -t devpts /dev/pts
 # Need to install ca-certificates from local debs before we can update apt.
 dpkg -i /scripts/deb/libssl1.1_1.1.1-1ubuntu2.1~18.04.5_amd64.deb
 dpkg -i /scripts/deb/openssl_1.1.1-1ubuntu2.1~18.04.5_amd64.deb
-dpkg -i /scripts/deb/ca-certificates_20180409_all.deb
+dpkg -i /scripts/deb/ca-certificates_20210119~18.04.2_all.deb
 
 apt-get update
 apt-get install -y systemd-sysv=237-3ubuntu10.43
