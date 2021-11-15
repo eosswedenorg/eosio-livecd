2aa7f0089e8dedc1e266d5faab2338daf9bf62be
Henrik Hautakoski <henrik@eossweden.org>
2021-11-15 18:25:50 +0100

    overlay/scripts/exit.sh: remove old stuffs.

--- overlay/scripts/exit.sh
+++ overlay/scripts/exit.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# If we changed some files that are stored in initrd. we need to regenerate it.
-# Just because i will forget to do this. we do it everytime :)
-#update-initramfs -u -k $(uname -r)
-
 truncate -s 0 /etc/machine-id
 rm /sbin/initctl
 dpkg-divert --rename --remove /sbin/initctl
