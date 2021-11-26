b5563f78294128b8e48a23a592a86fc6025f9a05
Henrik Hautakoski <henrik@eossweden.org>
2021-11-26 16:47:08 +0100

    overlay/scripts/conf.d/20-desktop.sh: install network-manager-gnome

--- overlay/scripts/conf.d/20-desktop.sh
+++ overlay/scripts/conf.d/20-desktop.sh
@@ -3,6 +3,7 @@
 # Window manager
 pkg-install --no-install-recommends \
     xubuntu-core=2.233 \
+    network-manager-gnome=1.8.24-1ubuntu3 \
     xfce4-statusnotifier-plugin=0.2.2-1 \
     xfce4-whiskermenu-plugin=2.4.3-1 \
     xfce4-power-manager-plugins=1.6.6-1 \
