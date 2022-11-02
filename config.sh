65f6b3fcd87693821bb97644466e7397411cfe90
Henrik Hautakoski <henrik@eossweden.org>
2022-11-02 16:07:11 +0100

    config.sh: bump version

--- config.sh
+++ config.sh
@@ -12,7 +12,7 @@ UBUNTU_MIRROR=http://se.archive.ubuntu.com
 TARGET_NAME=eosio-live
 
 # LiveCD version
-TARGET_VERSION=01
+TARGET_VERSION=02
 
 # Fullname used for output files.
 TARGET_FULLNAME="${TARGET_NAME}-${UBUNTU_VERSION}-${TARGET_VERSION}"
