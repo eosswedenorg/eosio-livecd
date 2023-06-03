84008882bde2438a036dac8c51761d5c544ac98f
Henrik Hautakoski <henrik@eossweden.org>
2023-06-03 19:07:25 +0200

    overlay/scripts/conf.d/30-eosio.sh: Comment in pkg-install causes bash to be confused sometimes. move it out.

--- overlay/scripts/conf.d/30-eosio.sh
+++ overlay/scripts/conf.d/30-eosio.sh
@@ -8,10 +8,10 @@ deb [arch=amd64] https://apt.eossweden.org/main focal stable
 deb [arch=amd64] https://apt.eossweden.org/eosio bionic stable
 EOF
 
+# libicu60 Needed for cleos-v2 for bionic. don't exist in focal repo.
 apt-get update
 pkg-install \
-    # Needed for cleos-v2 for bionic. don't exist in focal repo.
-    /scripts/deb/libicu60_60.2-3ubuntu3.2_amd64.deb
+    /scripts/deb/libicu60_60.2-3ubuntu3.2_amd64.deb \
     cleos-v2=1.8.4-rc1 \
 	eosio-keygen=1.0.7-1 \
     eosio-keygen-gui=1.0.7-1
