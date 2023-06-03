b5fa3a1f29984634b56e91f611d518e42e190a62
Henrik Hautakoski <henrik@eossweden.org>
2023-06-03 19:06:42 +0200

    overlay/scripts/conf.d/90-ufw.sh: Don't run ufw commands, instead update config files.
    Running ufw commands in chroot environment still uses host kernel modules etc (for iptables).
    Depending on what kernels the host is running and what kernel we have on the livecd, modprobe can't find the module.
    
    However, we dont need to run any ufw command and just update the config files.

--- overlay/scripts/conf.d/90-ufw.sh
+++ overlay/scripts/conf.d/90-ufw.sh
@@ -2,14 +2,9 @@
 
 pkg-install ufw=0.36-6ubuntu1
 
-yes | ufw enable
+config-install /etc/default/ufw
+config-install /etc/ufw/ufw.conf
+config-install /etc/ufw/user.rules
 
 # See https://github.com/husarnet/docker-example/issues/1
 update-alternatives --set ip6tables /usr/sbin/ip6tables-nft
-
-# deny outgoing trafic.
-ufw default deny outgoing
-
-# But allow HTTPS and DNS
-ufw allow out to any port 53 proto udp comment DNS
-ufw allow out to any port 443 proto tcp comment HTTPS
